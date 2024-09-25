fcb9272de53288ad6287bded5f0f51576582fc74
Wuelass <quentin.maury78230@gmail.com>
2024-09-25 11:11:08 +0200

    gin

--- main.go
+++ main.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"github.com/gin-gonic/gin"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+
+	// database.InitDatabase()
+
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+
+	})
+
+	// Start the server
+	router.Run(":8080")
+
+}
